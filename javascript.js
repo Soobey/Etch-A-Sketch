9a735f2a1fd612a22a9bc9f072f8c06a995ca8f7
Szabolcs Ambrus <a.sabi@gmx.de>
2022-04-28 17:49:21 +0200

    make grid more responsive

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('#grid');
-let n = 16;
+let n = 100;
 
 for (let i = 0; i < n**2; i++) {
     var div = document.createElement('div');
@@ -7,4 +7,6 @@ for (let i = 0; i < n**2; i++) {
     div.addEventListener('mousemove', (e) => {
         e.target.style.backgroundColor = 'black';
     });
+    div.style.width = (1000-n*2)/n + 'px';
+    div.style.height = (1000-n*2)/n + 'px';
 };
